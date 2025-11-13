0194d146373eae4cb22801a117fcfa4df226b0e6
Arjun Choubey <arjunchoubey+microsoft@microsoft.com>
2025-11-13 15:20:19 -0500

    Adding Envload

--- src/app/agents/customerLoyaltyAgent_initializer.py
+++ src/app/agents/customerLoyaltyAgent_initializer.py
@@ -7,8 +7,8 @@ from azure.ai.agents.models import FunctionTool, ToolSet
 from typing import Callable, Set, Any
 from tools.discountLogic import calculate_discount
 # from tools.aiSearchTools import product_data_ai_search
-#from dotenv import load_dotenv
-#load_dotenv()
+from dotenv import load_dotenv
+load_dotenv()
 #
 CL_PROMPT_TARGET = os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))), 'prompts', 'CustomerLoyaltyAgentPrompt.txt')
 with open(CL_PROMPT_TARGET, 'r', encoding='utf-8') as file:
