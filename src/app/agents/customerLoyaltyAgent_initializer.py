8fbb36c705e006b68eae1507aea900a21990f02d
Arjun Choubey <arjunchoubey+microsoft@microsoft.com>
2025-11-13 14:10:41 -0500

    After Agent Name Change

--- src/app/agents/customerLoyaltyAgent_initializer.py
+++ src/app/agents/customerLoyaltyAgent_initializer.py
@@ -7,14 +7,15 @@ from azure.ai.agents.models import FunctionTool, ToolSet
 from typing import Callable, Set, Any
 from tools.discountLogic import calculate_discount
 # from tools.aiSearchTools import product_data_ai_search
-from dotenv import load_dotenv
-load_dotenv()
+#from dotenv import load_dotenv
+#load_dotenv()
 #
 CL_PROMPT_TARGET = os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))), 'prompts', 'CustomerLoyaltyAgentPrompt.txt')
 with open(CL_PROMPT_TARGET, 'r', encoding='utf-8') as file:
     CL_PROMPT = file.read()
 #
 project_endpoint= os.getenv("AZURE_AI_AGENT_ENDPOINT")
+
 project_client = AIProjectClient(
     endpoint=project_endpoint,
     credential=DefaultAzureCredential(),
