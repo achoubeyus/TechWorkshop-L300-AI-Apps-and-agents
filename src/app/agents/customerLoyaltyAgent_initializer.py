e968399557f9aa9feb385ed7cdae34c1424aa3a7
Arjun Choubey <arjunchoubey+microsoft@microsoft.com>
2025-11-14 09:06:04 -0500

    CL Deplyoment With KF Code

--- src/app/agents/customerLoyaltyAgent_initializer.py
+++ src/app/agents/customerLoyaltyAgent_initializer.py
@@ -1,6 +1,5 @@
 import os
 import sys
-
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from azure.ai.projects import AIProjectClient
 from azure.identity import DefaultAzureCredential
@@ -8,35 +7,30 @@ from azure.ai.agents.models import FunctionTool, ToolSet
 from typing import Callable, Set, Any
 from tools.discountLogic import calculate_discount
 # from tools.aiSearchTools import product_data_ai_search
-
 from dotenv import load_dotenv
 load_dotenv()
-#
+
 CL_PROMPT_TARGET = os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))), 'prompts', 'CustomerLoyaltyAgentPrompt.txt')
 with open(CL_PROMPT_TARGET, 'r', encoding='utf-8') as file:
     CL_PROMPT = file.read()
-#
-project_endpoint= "https://aif-teyafqh5v6hne.services.ai.azure.com/api/projects/proj-teyafqh5v6hne"
-agent_id = "asst_EiXitJbekhyM6EMuvuHaWze2"
-#https://aif-teyafqh5v6hne.services.ai.azure.com/api/projects/proj-teyafqh5v6hne
-#endpoint = os.environ.get("AZURE_AI_PROJECT_ENDPOINT")
 
+project_endpoint= os.getenv("AZURE_AI_AGENT_ENDPOINT")
 project_client = AIProjectClient(
     endpoint=project_endpoint,
     credential=DefaultAzureCredential(),
 )
-#
+
+agent_id = os.environ["customer_loyalty"]
+
 user_functions: Set[Callable[..., Any]] = {
     calculate_discount,
 }
-#
+
 # Initialize agent toolset with user functions
 functions = FunctionTool(user_functions)
 toolset = ToolSet()
 toolset.add(functions)
 project_client.agents.enable_auto_function_calls(tools=functions)
-
- # Create the agent using a specific deployment, name, instructions, and toolset
 
 with project_client:
     agent_exists = False
@@ -49,17 +43,18 @@ with project_client:
     if agent_exists:
         agent = project_client.agents.update_agent(
             agent_id=agent.id,
-            model=os.getenv("AZURE_AI_AGENT_MODEL_DEPLOYMENT_NAME"),  # Model deployment name
-            name="customer_loyalty",  # Name of the agent
-            instructions=CL_PROMPT,  # Instructions for the agent
+            model=os.environ["AZURE_AI_AGENT_MODEL_DEPLOYMENT_NAME"],  # Model deployment name
+            name="Zava Customer Loyalty Agent",  # Name of the agent
+            instructions=CL_PROMPT,  # Updated instructions for the agent
             # toolset=toolset
         )
         print(f"Updated agent, ID: {agent.id}")
     else:
         agent = project_client.agents.create_agent(
-            model=os.getenv("AZURE_AI_AGENT_MODEL_DEPLOYMENT_NAME"),  # Model deployment name
-            name="customer_loyalty",  # Name of the agent
+            model=os.environ["AZURE_AI_AGENT_MODEL_DEPLOYMENT_NAME"],  # Model deployment name
+            name="Zava Customer Loyalty Agent",  # Name of the agent
             instructions=CL_PROMPT,  # Instructions for the agent
             # toolset=toolset
         )
         print(f"Created agent, ID: {agent.id}")
+ 
